b86a359be0fcca82822f42df9593ba5d25197b17
Khai Phan Van <phan_van_khai123@yahoo.com>
2021-05-20 07:47:33 +0700

    BUOI9 17 5 2021

--- Article.java
+++ Article.java
@@ -1,75 +1,120 @@
 package coeding.java.blog;
 
+import java.util.Date;
+
 /**
  * 
  * has info about book
  * 
  * 	make getter, setter
  * 		tính đóng gói	encapsulation
+ * 
+ *  blog contents Modeling and Relation <- R DBMS
  * */
 public class Article {
-	// Access Modifier : private public protected (default)
-	// field have to be private
-	// method public
-	//private: ngăn class bên ngoài sd
-	//field static đều mang một giá trị
-	private long seq;
-	private String title;
-	private String writerName;
-	private String content;
+	/**
+	 * Access Modifier : private public protected (default)
+	 * field have to be private
+	 * method public
+	 * private: ngăn class bên ngoài sd
+	 * field static đều mang một giá trị
+	 * */
+    private Long aid;
+    private String title;
+    private String content;
+    private String name;
+    private Date wdate;
 	public Article() {}
 	
+	public Long getAid() {
+		return aid;
+	}
+
+	public void setAid(Long aid) {
+		this.aid = aid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getWdate() {
+		return wdate;
+	}
+
+	public void setWdate(Date wdate) {
+		this.wdate = wdate;
+	}
+	
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
 	//make clone method as Constructor
 	//Copy constructor		khi tạo constructor thì default biến mất
 	public Article(Article arg) {
+		
 		// this mean new instance
-		this.seq = arg.getSeq();
-		this.title = arg.getTitle();
-		this.writerName = arg.getWriterName();
-		this.content = arg.getContent();
+//		this.seq = arg.getSeq();
+//		this.title = arg.getTitle();
+//		this.writerName = arg.getWriterName();
+//		this.content = arg.getContent();
 	}
 	
 	// has all argument for fields
 	// constructor using Fields
-	public Article(long seq, String title, String writerName, String content) {
+	public Article(Long aid, String title, String content, String name, Date wdate) {
 		super();
-		this.seq = seq;
+		this.aid = aid;
 		this.title = title;
-		this.writerName = writerName;
 		this.content = content;
+		this.name = name;
+		this.wdate = wdate;
 	}
 	//khi ko sd constructor
-	public Article copyTo(Article arg) {
-		Article article = new Article();
-		article.setSeq(arg.getSeq());
-		article.setTitle(arg.getTitle());;
-		article.setWriterName(arg.getWriterName());;
-		article.setContent(arg.getContent());
-		return article;
+//	public Article copyTo(Article arg) {
+//		Article article = new Article();
+//		article.setSeq(arg.getSeq());
+//		article.setTitle(arg.getTitle());;
+//		article.setWriterName(arg.getWriterName());;
+//		article.setContent(arg.getContent());
+//		return article;
+//	}
+	
+	// Article 모델 Copy
+	public void CopyData(Article param) {
+		this.aid = param.getAid();
+        this.title = param.getTitle();
+        this.content = param.getContent();
+        this.name = param.getName();
+        this.wdate = param.getWdate();
 	}
-	public long getSeq() {
-		return seq;
-	}
-	public void setSeq(long seq) {
-		this.seq = seq;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public String getWriterName() {
-		return writerName;
-	}
-	public void setWriterName(String writerName) {
-		this.writerName = writerName;
-	}
-	public String getContent() {
-		return content;
-	}
-	public void setContent(String content) {
-		this.content = content;
-	}
+
 	// alt+shift+s > generate Getter/Setter
+
+	@Override
+	public String toString() {
+		return "Article [aid=" + aid + ", title=" + title + ", content=" + content + ", name=" + name + ", wdate="
+				+ wdate + "]";
+	}
+
+	
+	
 }
---
+++ impl/ArticleDAO.java
@@ -0,0 +1,83 @@
+package coeding.java.blog.impl;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import coeding.java.blog.Article;
+import coeding.java.blog.DAO;
+
+//method' rule like desgin for project
+public class ArticleDAO implements DAO<Article>{
+	
+	private List<Article> storage;// in Memory->>>>> in FILE
+	//private ArticleDAOFile;
+	
+	//what data type?
+	
+	// default constructor : must has
+	public ArticleDAO() {
+		storage = new ArrayList<Article>();
+		System.out.println("created "+this.getClass().getName()); //Lấy thông tin class
+	}
+	
+	@Override
+	public void delete(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public List<Article> select(Article arg) {// all articles
+		// TODO Auto-generated method stub
+		if(arg == null) {
+			return new ArrayList<Article>(storage);//like clone
+			//return storage.subList(0, 0);	//all return	
+			//all return not will, .size() == 0
+			//subList(0, 0); copied
+		}
+		
+		List<Article> rs = new ArrayList<Article>();
+		if(arg.getName() != null && !arg.getName().isEmpty()) {
+			//.length > 0 not arg.getName().isEmpty()
+			//searching name
+			for(Article at : storage) {
+				if(at.getName().equals(arg.getName().isEmpty())) {
+					rs.add(at);// name is not unique dont break;
+				}
+			}
+		}
+		if(arg.getAid() != null) {///Type Long
+			for (Article at : storage) {
+				if(at.getAid() == arg.getAid()) {
+					/**
+					 *  1. Long is class not primitive .equal()
+					 *	but , wrapping (long)	<----- auto convert to long
+					 *	2. at where save ?
+					 *	if aid is unique ()
+					 */
+					rs.add(at);
+					break;
+				}
+			}
+		}
+		return rs;
+	}
+
+	@Override
+	public void insert(Article arg) {//void không trả về giá trị
+		// TODO Auto-generated method stub
+		//--->	int lastIndex = storage.size()-1;
+		//or
+		// aid be auto increments when add
+		arg.setAid((Long)(storage.size() + 1L));
+		storage.add(arg);
+//		System.err.println(storage);
+	}
+
+	@Override
+	public void update(Article arg) {
+		// TODO Auto-generated method stub
+		
+	}
+}
